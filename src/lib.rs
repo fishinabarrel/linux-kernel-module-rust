48acd06a4a58a04de2e08ae2420864d81dfa4c4f
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-13 12:11:45 -0400

    It... compiles?

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,41 @@
+pub mod types;
+
 #[macro_export]
 macro_rules! kernel_module {
     ($module:ty, $($name:ident : $value:expr),* ) => (
+        use $crate::KernelModule;
+        static mut __MOD: Option<$module> = None;
+        #[no_mangle]
+        pub fn init_module() -> $crate::types::c_int {
+            match <$module as $crate::KernelModule>::init() {
+                Ok(m) => {
+                    unsafe {
+                        __MOD = Some(m);
+                    }
+                    return 0;
+                },
+                Err(e) => {
+                    return e.to_kernel_errno();
+                }
+            }
+        }
 
+        #[no_mangle]
+        pub fn module_exit() {
+            unsafe {
+                __MOD.as_mut().unwrap().exit();
+            }
+        }
     );
 }
 
 pub enum Error {
+}
 
+impl Error {
+    pub fn to_kernel_errno(&self) -> types::c_int {
+        return 0;
+    }
 }
 
 pub trait KernelModule : Sized {
---
+++ src/types.rs
@@ -0,0 +1,3 @@
+#![allow(non_camel_case_types)]
+
+pub type c_int = i32;
