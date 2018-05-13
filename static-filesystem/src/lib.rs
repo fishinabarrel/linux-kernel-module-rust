496ba3f59581d950221fe547d344002c8f12b753
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-13 11:35:53 -0400

    Minimum consumption to init something

--- static-filesystem/src/lib.rs
+++ static-filesystem/src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use]
+extern crate linux_kernel_module;
+
+struct StaticFileSystem;
+
+impl KernelModule for StaticFileSystem {
+    fn init() -> Result<(), ()> {
+
+    }
+
+    fn exit() {
+
     }
 }
+
+kernel_module!(StaticFileSystem);
