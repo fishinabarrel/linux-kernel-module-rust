b6ab24d0adf9b766a5c1d5b8cc5d6e3933c362f5
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-13 20:55:20 -0400

    Explicitly list what is included in bindgen

--- build.rs
+++ build.rs
@@ -3,10 +3,25 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
+const INCLUDED_TYPES: &[&str] = &[];
+const INCLUDED_FUNCTIONS: &[&str] = &[];
+const INCLUDED_VARS: &[&str] = &[];
+
 fn main() {
-    let bindings = bindgen::Builder::default()
+    let mut builder = bindgen::Builder::default()
         // TODO: what header for linux!
-        .header("XXX!")
+        .header("XXX!");
+
+    for t in INCLUDED_TYPES {
+        builder = builder.whitelist_type(t);
+    }
+    for f in INCLUDED_FUNCTIONS {
+        builder = builder.whitelist_function(f);
+    }
+    for v in INCLUDED_VARS {
+        builder = builder.whitelist_var(v);
+    }
+    let bindings = builder
         .generate()
         .expect("Unable to generate bindings");
 
