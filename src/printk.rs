b0978fe2f552cac50842b475e37da02c9185f252
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-15 19:30:32 -0400

    Readability improvement

--- src/printk.rs
+++ src/printk.rs
@@ -16,18 +16,20 @@ impl fmt::Write for KernelConsole {
     }
 }
 
+const KERN_INFO: &str = "\x016";
+
 #[macro_export]
 macro_rules! println {
     () => ({
         use ::core::fmt::Write;
-        let _ = $crate::printk::KernelConsole.write_str("\x016\n");
+        let _ = $crate::printk::KernelConsole.write_str(KERN_INFO);
     });
     ($fmt:expr) => ({
         use ::core::fmt::Write;
-        let _ = $crate::printk::KernelConsole.write_str(concat!("\x016", $fmt, "\n"));
+        let _ = $crate::printk::KernelConsole.write_str(concat!(KERN_INFO, $fmt, "\n"));
     });
     ($fmt:expr, $($arg:tt)*) => ({
         use ::core::fmt::Write;
-        let _ = $crate::printk::KernelConsole.write_fmt(format_args!(concat!("\x016", $fmt, "\n"), $($arg)*));
+        let _ = $crate::printk::KernelConsole.write_fmt(format_args!(concat!(KERN_INFO, $fmt, "\n"), $($arg)*));
     });
 }
