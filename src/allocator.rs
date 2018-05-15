65aa8b2cd6dcd9e678eb46d1cb5f6fc397b337f4
Geoffrey Thomas <geofft@ldpreload.com>
2018-05-15 19:42:49 +0000

    work around bindgen

--- src/allocator.rs
+++ src/allocator.rs
@@ -5,9 +5,12 @@ use types;
 
 pub struct KernelAllocator;
 
+// bindgen problems
+const GFP_KERNEL: types::c_int = 0xc000c0;
+
 unsafe impl<'a> Alloc for &'a KernelAllocator {
     unsafe fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
-        let ptr = bindings::krealloc(0 as *const types::c_void, layout.size(), bindings::GFP_KERNEL) as *mut u8;
+        let ptr = bindings::krealloc(0 as *const types::c_void, layout.size(), GFP_KERNEL) as *mut u8;
         if ptr.is_null() {
             return Err(AllocErr::Exhausted{request: layout});
         }
