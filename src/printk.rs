9ebadc78f104387b85914f0a771e5fa97b1a1c03
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-18 08:27:20 -0400

    Fixed #2 -- refactored printk to not need to malloc
    Instead we make multiple calls to printk, leveraging KERNEL_CONT. In the common case we still only have a single call to it.

--- src/printk.rs
+++ src/printk.rs
@@ -5,13 +5,23 @@ use types::c_int;
 pub struct KernelConsole;
 
 extern "C" {
-    fn printk_helper(s: *const u8, len: c_int) -> c_int;
+    fn printk_info_helper(s: *const u8, len: c_int) -> c_int;
+    fn printk_cont_helper(s: *const u8, len: c_int) -> c_int;
+}
+
+fn printk_info(s: &str) {
+    // TODO: I believe printk never fails
+    unsafe { printk_info_helper(s.as_ptr(), s.len() as c_int) };
+}
+
+fn printk_cont(s: &str) {
+    // TODO: I believe printk never fails
+    unsafe { printk_cont_helper(s.as_ptr(), s.len() as c_int) };
 }
 
 impl fmt::Write for KernelConsole {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        // TODO: I believe printk never fails
-        unsafe { printk_helper(s.as_ptr(), s.len() as c_int) };
+        printk_cont(s);
         Ok(())
     }
 }
@@ -19,17 +29,14 @@ impl fmt::Write for KernelConsole {
 #[macro_export]
 macro_rules! println {
     () => ({
-        use ::core::fmt::Write;
-        let _ = $crate::printk::KernelConsole.write_str("\x016\n");
+        $crate::printk::printk_info("\n");
     });
     ($fmt:expr) => ({
-        use ::core::fmt::Write;
-        let _ = $crate::printk::KernelConsole.write_str(concat!("\x016", $fmt, "\n"));
+        $crate::printk::printk_info(concat!($fmt, "\n"));
     });
     ($fmt:expr, $($arg:tt)*) => ({
         use ::core::fmt::Write;
-        // TODO: Don't allocate!
-        let s = format!(concat!("\x016", $fmt, "\n"), $($arg)*);
-        let _ = $crate::printk::KernelConsole.write_str(&s);
+        $crate::printk::printk_info("");
+        let _ = $crate::printk::KernelConsole.write_fmt(format_args!(concat!($fmt, "\n"), $($args)*);
     });
 }
