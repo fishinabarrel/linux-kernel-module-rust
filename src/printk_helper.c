c9470b8a20aec59fc219bafd131cf6af4dc3bf36
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-18 08:21:40 -0400

    Expose dedicated functions for kernel info and kernel cont prints

--- src/printk_helper.c
+++ src/printk_helper.c
@@ -1,6 +1,11 @@
 #include <linux/printk.h>
 
-int printk_helper(const unsigned char *s, int len)
+int printk_info_helper(const unsigned char *s, int len)
 {
-	return printk("%.*s", len, (const char *)s);
+	return printk(KERNEL_INFO "%.*s", len, (const char *)s);
+}
+
+int printk_cont_helper(const unsigned char *s, int len)
+{
+	return printk(KERNEL_CONT "%.*s", len, (const char *)s);
 }
