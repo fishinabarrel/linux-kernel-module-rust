4cf8f8421638eebf1374d47a6e7702ba0b6335fb
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-27 22:21:00 +0000

    forward progress

--- src/sysctl.rs
+++ src/sysctl.rs
@@ -1,9 +1,10 @@
 use alloc::boxed::Box;
-use alloc::string::ToString;
 use alloc::vec;
+use core::mem;
 use core::ptr;
 
 use bindings;
+use c_types;
 use error;
 use types;
 
@@ -13,20 +14,48 @@ pub struct Sysctl<T: Sync> {
     header: *mut bindings::ctl_table_header,
 }
 
+unsafe extern "C" fn proc_handler<T>(
+    ctl: *mut bindings::ctl_table,
+    write: c_types::c_int,
+    buffer: *mut c_types::c_void,
+    len: *mut usize,
+    ppos: *mut bindings::loff_t,
+) -> c_types::c_int {
+    unimplemented!();
+}
+
 impl<T: Sync> Sysctl<T> {
-    pub fn register(path: &str, storage: T, mode: types::Mode) -> error::KernelResult<Sysctl<T>> {
-        let namespace_pos = path.rfind('/').ok_or(error::Error::EINVAL)?;
-        let (namespace, name) = path.split_at(namespace_pos);
-        let namespace = namespace.to_string() + "\x00";
-        let name = name.to_string() + "\x00";
+    pub fn register(
+        path: &'static str,
+        name: &'static str,
+        storage: T,
+        mode: types::Mode,
+    ) -> error::KernelResult<Sysctl<T>> {
+        if !path.ends_with('\x00') || !name.ends_with('\x00') || name.contains('\x00') {
+            return Err(error::Error::EINVAL);
+        }
 
-        let storage = Box::new(storage);
+        let mut storage = Box::new(storage);
+        let mut table = vec![
+            bindings::ctl_table {
+                procname: name.as_ptr() as *const i8,
+                mode: mode.as_int(),
+                data: &mut *storage as *mut T as *mut c_types::c_void,
+                proc_handler: Some(proc_handler::<T>),
 
-        let table = vec![bindings::ctl_table { name }, bindings::ctl_table {}].into_boxed_slice();
+                maxlen: 0,
+                child: ptr::null_mut(),
+                poll: ptr::null_mut(),
+                extra1: ptr::null_mut(),
+                extra2: ptr::null_mut(),
+            },
+            unsafe { mem::zeroed() },
+        ].into_boxed_slice();
 
-        let result = bindings::register_sysctl(namespace, table);
+        let result =
+            unsafe { bindings::register_sysctl(path.as_ptr() as *const i8, table.as_mut_ptr()) };
         if result.is_null() {
-            return Err(error::Error::XXX);
+            return Err(error::Error::ENOMEM);
         }
 
         return Ok(Sysctl {
@@ -43,7 +72,9 @@ impl<T: Sync> Sysctl<T> {
 
 impl<T: Sync> Drop for Sysctl<T> {
     fn drop(&mut self) {
-        bindings::unregister_sysctl_table(self.header);
+        unsafe {
+            bindings::unregister_sysctl_table(self.header);
+        }
         self.header = ptr::null_mut();
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,13 @@
 use bindings;
 
-pub struct Mode(bindings::mode_t);
+pub struct Mode(bindings::umode_t);
 
 impl Mode {
-    pub fn from_int(m: u32) -> Mode {
+    pub fn from_int(m: u16) -> Mode {
         Mode(m)
+    }
+
+    pub fn as_int(&self) -> u16 {
+        return self.0;
     }
 }
