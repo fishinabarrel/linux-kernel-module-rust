56e8cd3a198a330971c75e6b7d2f73823fbf8c65
Geoffrey Thomas <geofft@ldpreload.com>
2018-05-27 20:50:05 -0400

    apply tiny diff from master

--- src/helpers.c
+++ src/helpers.c
@@ -1,6 +1,12 @@
+#include <linux/bug.h>
 #include <linux/printk.h>
 
 int printk_helper(const unsigned char *s, int len)
 {
-	return printk(KERN_INFO "%.*s", len, (const char *)s);
+    return printk(KERN_INFO "%.*s", len, (const char *)s);
+}
+
+void bug_helper(void)
+{
+    BUG();
 }
