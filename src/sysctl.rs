7eb71f48c82c5677d430f376754fd0697f77c9ca
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-30 23:38:48 +0000

    Almost there!

--- src/sysctl.rs
+++ src/sysctl.rs
@@ -21,9 +21,30 @@ pub trait SysctlStorage: Sync {
     fn read_value(&self, data: &mut UserSlicePtrWriter) -> (usize, error::KernelResult<()>);
 }
 
+fn trim_whitespace(mut data: &[u8]) -> &[u8] {
+    while !data.is_empty() && (data[0] == b' ' || data[0] == b'\t' || data[0] == b'\n') {
+        data = &data[1..];
+    }
+    while !data.is_empty() && (data[data.len() - 1] == b' ' || data[data.len() -1] == b'\t' || data[data.len() - 1] == b'\n') {
+        data = &data[..data.len() - 2];
+    }
+    return data;
+}
+
 impl SysctlStorage for atomic::AtomicBool {
     fn store_value(&self, data: &[u8]) -> (usize, error::KernelResult<()>) {
-        unimplemented!();
+         let result = match trim_whitespace(data) {
+             b"0" => {
+                 self.store(false, atomic::Ordering::Relaxed);
+                 Ok(())
+             },
+             b"1" => {
+                 self.store(true, atomic::Ordering::Relaxed);
+                 Ok(())
+             },
+             _ => Err(error::Error::EINVAL)
+        };
+        return (data.len(), result);
     }
 
     fn read_value(&self, data: &mut UserSlicePtrWriter) -> (usize, error::KernelResult<()>) {
