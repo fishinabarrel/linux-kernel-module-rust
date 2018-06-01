fab9789b1665480761bbe64ccdf93eb4a429f7af
Alex Gaynor <alex.gaynor@gmail.com>
2018-05-31 22:08:49 -0400

    Added two more error constants

--- src/error.rs
+++ src/error.rs
@@ -5,6 +5,8 @@ pub struct Error(c_types::c_int);
 
 impl Error {
     pub const EINVAL: Self = Error(-(bindings::EINVAL as i32));
+    pub const ENOMEM: Self = Error(-(bindings::ENOMEM as i32));
+    pub const EFAULT: Self = Error(-(bindings::EFAULT as i32));
 
     pub fn from_kernel_errno(errno: c_types::c_int) -> Error {
         return Error(errno);
