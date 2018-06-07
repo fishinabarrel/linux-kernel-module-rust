cf2be8a06d84058bf4fa20ff993a1d2bc3a9a6d8
Alex Gaynor <alex.gaynor@gmail.com>
2018-06-07 00:24:04 +0000

    Fill out this function

--- tests/printk/tests.rs
+++ tests/printk/tests.rs
@@ -1,5 +1,33 @@
+use std::process::Command;
+
+struct LoadedModule {
+    name: &'static str,
+}
+
+impl LoadedModule {
+    fn load(name: &'static str) -> LoadedModule {
+        Command::new("sudo")
+            .arg("insmod")
+            .arg(name)
+            .spawn()
+            .unwrap();
+        return LoadedModule { name };
+    }
+}
+
+impl Drop for LoadedModule {
+    fn drop(&mut self) {
+        Command::new("sudo")
+            .arg("rmmod")
+            .arg(self.name)
+            .spawn()
+            .unwrap();
+    }
+}
+
 fn with_kernel_module<F: Fn()>(f: F) {
-    unimplemented!();
+    let _m = LoadedModule::load("testmodule.ko");
+    f();
 }
 
 fn assert_dmesg_contains(msgs: &[&str]) {
@@ -9,8 +37,6 @@ fn assert_dmesg_contains(msgs: &[&str]) {
 #[test]
 fn test_printk() {
     with_kernel_module(|| {
-        assert_dmesg_contains(&[
-            "Single element printk", "", "printk with 2 parameters!"
-        ]);
+        assert_dmesg_contains(&["Single element printk", "", "printk with 2 parameters!"]);
     });
 }
