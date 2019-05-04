26d6d697441680d2721af46336ebe09384278471
Daniel Kolsoi <ThaDan64@gmail.com>
2019-05-04 15:03:42 -0400

    Add TODO to kernel module macro

--- src/lib.rs
+++ src/lib.rs
@@ -60,6 +60,8 @@ macro_rules! kernel_module {
         #[link_section = ".modinfo"]
         #[allow(non_upper_case_globals)]
         // TODO: Generate a name the same way the kernel's `__MODULE_INFO` does.
+        // TODO: This needs to be a `[u8; _]`, since the kernel defines this as a  `const char []`.
+        // See https://github.com/rust-lang/rfcs/pull/2545
         pub static $name: &'static [u8] = concat!(stringify!($name), "=", $value, '\0').as_bytes();
     };
 }
