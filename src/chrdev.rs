d8c6e90d8e16de6d0ab79dfca026e25a9e827c62
Alex Gaynor <alex.gaynor@gmail.com>
2019-05-06 15:29:46 -0400

    Refs #55 -- added API for allocating character device numbers (#59)

---
+++ src/chrdev.rs
@@ -0,0 +1,44 @@
+use core::ops::Range;
+
+use bindings;
+use c_types;
+use error;
+
+pub struct DeviceNumberRegion {
+    dev: bindings::dev_t,
+    count: usize,
+}
+
+impl DeviceNumberRegion {
+    pub fn allocate(
+        minors: Range<usize>,
+        name: &'static str,
+    ) -> error::KernelResult<DeviceNumberRegion> {
+        if !name.ends_with('\x00') {
+            return Err(error::Error::EINVAL);
+        }
+
+        let count = minors.end - minors.start;
+        let mut dev: bindings::dev_t = 0;
+        let res = unsafe {
+            bindings::alloc_chrdev_region(
+                &mut dev,
+                minors.start as bindings::dev_t,
+                count as bindings::dev_t,
+                name.as_ptr() as *const c_types::c_char,
+            )
+        };
+        if res != 0 {
+            return Err(error::Error::from_kernel_errno(res));
+        }
+        return Ok(DeviceNumberRegion { dev, count });
+    }
+}
+
+impl Drop for DeviceNumberRegion {
+    fn drop(&mut self) {
+        unsafe {
+            bindings::unregister_chrdev_region(self.dev, self.count as bindings::dev_t);
+        }
+    }
+}
---
+++ tests/chrdev/src/lib.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![feature(const_str_as_bytes)]
+
+#[macro_use]
+extern crate linux_kernel_module;
+
+struct ChrdevTestModule {
+    _dev: linux_kernel_module::chrdev::DeviceNumberRegion,
+}
+
+impl linux_kernel_module::KernelModule for ChrdevTestModule {
+    fn init() -> linux_kernel_module::KernelResult<Self> {
+        Ok(ChrdevTestModule {
+            _dev: linux_kernel_module::chrdev::DeviceNumberRegion::allocate(
+                0..1,
+                "chrdev-tests\x00",
+            )?,
+        })
+    }
+}
+
+kernel_module!(
+    ChrdevTestModule,
+    author: "Alex Gaynor and Geoffrey Thomas",
+    description: "A module for testing character devices",
+    license: "GPL"
+);
