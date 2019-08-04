8f3578086d13d81c0b375c453daef10e35fc5cbf
Alex Gaynor <alex.gaynor@gmail.com>
2019-08-03 20:08:19 -0400

    Support multiple devices in the chrdev tests

--- tests/chrdev/tests/tests.rs
+++ tests/chrdev/tests/tests.rs
@@ -1,9 +1,11 @@
+use std::fs;
 use std::io::Read;
 use std::path::PathBuf;
 use std::process::Command;
-use std::{env, fs};
 
 use libc;
+
+use tempfile::TempDir;
 
 use kernel_module_testlib::with_kernel_module;
 
@@ -20,8 +22,8 @@ fn get_device_major_number() -> libc::dev_t {
 }
 
 fn temporary_file_path() -> PathBuf {
-    let mut p = env::temp_dir();
-    p.push("chrdev-test-device");
+    let mut p = TempDir::new().unwrap().into_path();
+    p.push("device");
     return p;
 }
 
@@ -39,23 +41,25 @@ impl Drop for UnlinkOnDrop<'_> {
     }
 }
 
-fn mknod(path: &PathBuf, device_number: libc::dev_t) -> UnlinkOnDrop {
+fn mknod(path: &PathBuf, major: libc::dev_t, minor: libc::dev_t) -> UnlinkOnDrop {
     Command::new("sudo")
         .arg("mknod")
         .arg(path.to_str().unwrap())
         .arg("c")
-        .arg(device_number.to_string())
-        .arg("0")
+        .arg(major.to_string())
+        .arg(minor.to_string())
         .status()
         .unwrap();
     return UnlinkOnDrop { path };
 }
 
+const READ_FILE_MINOR: libc::dev_t = 0;
+
 #[test]
 fn test_mknod() {
     with_kernel_module(|| {
         let device_number = get_device_major_number();
-        mknod(&temporary_file_path(), device_number);
+        mknod(&temporary_file_path(), device_number, READ_FILE_MINOR);
     });
 }
 
@@ -64,7 +68,7 @@ fn test_read() {
     with_kernel_module(|| {
         let device_number = get_device_major_number();
         let p = temporary_file_path();
-        let _u = mknod(&p, device_number);
+        let _u = mknod(&p, device_number, READ_FILE_MINOR);
 
         let mut f = fs::File::open(&p).unwrap();
         let mut data = [0; 12];
