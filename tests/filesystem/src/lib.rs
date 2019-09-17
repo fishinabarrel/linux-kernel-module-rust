10992f3bd433f2fe8ec9fe932b82260e40044a0d
Luis Gerhorst <luis.gerhorst@fau.de>
2019-09-17 16:38:30 +0200

    Add tests for filesystem::register
    Check whether loading the module makes the name of the filesystem
    (testfs) appear in /proc/filesystems

---
+++ tests/filesystem/src/lib.rs
@@ -0,0 +1,38 @@
+#![no_std]
+#![feature(const_str_as_bytes)]
+#![feature(const_raw_ptr_deref)]
+
+extern crate alloc;
+
+use linux_kernel_module::filesystem::{self, FileSystem, FileSystemFlags};
+use linux_kernel_module::{self, CStr};
+
+struct TestFSModule {
+    _fs_registration: filesystem::Registration<TestFS>,
+}
+
+// This is safe because Registration doesn't actually expose any methods.
+unsafe impl Sync for TestFSModule {}
+
+struct TestFS {}
+
+impl FileSystem for TestFS {
+    const NAME: &'static CStr = unsafe { &*("testfs\x00" as *const str as *const CStr) };
+    const FLAGS: FileSystemFlags = FileSystemFlags::FS_REQUIRES_DEV;
+}
+
+impl linux_kernel_module::KernelModule for TestFSModule {
+    fn init() -> linux_kernel_module::KernelResult<Self> {
+        let fs_registration = filesystem::register::<TestFS>()?;
+        Ok(TestFSModule {
+            _fs_registration: fs_registration,
+        })
+    }
+}
+
+linux_kernel_module::kernel_module!(
+    TestFSModule,
+    author: "Fish in a Barrel Contributors",
+    description: "A module for testing filesystem::register",
+    license: "GPL"
+);
