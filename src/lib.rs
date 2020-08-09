1c843e43a36def757c75406c26f527ace64e3f9a
Alex Gaynor <alex.gaynor@gmail.com>
2020-08-08 21:38:27 -0400

    Attempt to get the type correct on this attribute

--- src/lib.rs
+++ src/lib.rs
@@ -76,9 +76,7 @@ macro_rules! kernel_module {
         #[link_section = ".modinfo"]
         #[allow(non_upper_case_globals)]
         // TODO: Generate a name the same way the kernel's `__MODULE_INFO` does.
-        // TODO: This needs to be a `[u8; _]`, since the kernel defines this as a  `const char []`.
-        // See https://github.com/rust-lang/rfcs/pull/2545
-        pub static $name: &'static [u8] = concat!(stringify!($name), "=", $value, '\0').as_bytes();
+        pub static $name: [u8; concat!(stringify!($name), "=", $value, '\0').len()] = *concat!(stringify!($name), "=", $value, '\0').as_bytes();
     };
 }
 
