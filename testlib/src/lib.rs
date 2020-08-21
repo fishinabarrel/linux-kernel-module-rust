25b3512c953f20504f1cab815a18585583e3d24c
Geoffrey Thomas <geofft@ldpreload.com>
2020-08-21 09:15:07 -0400

    tests: Move device-node code into testlib

--- testlib/src/lib.rs
+++ testlib/src/lib.rs
@@ -1,5 +1,9 @@
 use std::env;
+use std::fs;
+use std::path::PathBuf;
 use std::process::Command;
+
+use tempfile::TempDir;
 
 struct LoadedModule {
     name: String,
@@ -49,4 +53,46 @@ pub fn assert_dmesg_contains(msgs: &[&[u8]]) {
         assert!(pos.is_some());
         lines = &lines[pos.unwrap()..];
     }
+}
+
+pub fn get_device_major_number(name: &str) -> libc::dev_t {
+    let devices = fs::read_to_string("/proc/devices").unwrap();
+    let dev_no_line = devices.lines().find(|l| l.ends_with(name)).unwrap();
+    let elements = dev_no_line.rsplitn(2, " ").collect::<Vec<_>>();
+    assert_eq!(elements.len(), 2);
+    assert_eq!(elements[0], name);
+    return elements[1].trim().parse().unwrap();
+}
+
+pub fn temporary_file_path() -> PathBuf {
+    let mut p = TempDir::new().unwrap().into_path();
+    p.push("device");
+    return p;
+}
+
+pub struct UnlinkOnDrop<'a> {
+    path: &'a PathBuf,
+}
+
+impl Drop for UnlinkOnDrop<'_> {
+    fn drop(&mut self) {
+        Command::new("sudo")
+            .arg("rm")
+            .arg(self.path.to_str().unwrap())
+            .status()
+            .unwrap();
+    }
+}
+
+pub fn mknod(path: &PathBuf, major: libc::dev_t, minor: libc::dev_t) -> UnlinkOnDrop {
+    Command::new("sudo")
+        .arg("mknod")
+        .arg("--mode=a=rw")
+        .arg(path.to_str().unwrap())
+        .arg("c")
+        .arg(major.to_string())
+        .arg(minor.to_string())
+        .status()
+        .unwrap();
+    return UnlinkOnDrop { path };
 }
