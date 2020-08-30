079f100273f76c9edf53103964bd8deb83ce0b7d
Alex Gaynor <alex.gaynor@gmail.com>
2020-08-29 22:11:50 -0400

    Change the FileOperations API to reduce the use of nightly features (#268)

--- src/chrdev.rs
+++ src/chrdev.rs
@@ -23,7 +23,7 @@ pub fn builder(name: &'static CStr, minors: Range<u16>) -> KernelResult<Builder>
 pub struct Builder {
     name: &'static CStr,
     minors: Range<u16>,
-    file_ops: Vec<&'static file_operations::FileOperationsVtable>,
+    file_ops: Vec<&'static bindings::file_operations>,
 }
 
 impl Builder {
@@ -31,7 +31,8 @@ impl Builder {
         if self.file_ops.len() >= self.minors.len() {
             panic!("More devices registered than minor numbers allocated.")
         }
-        self.file_ops.push(&T::VTABLE);
+        self.file_ops
+            .push(&file_operations::FileOperationsVtable::<T>::VTABLE);
         self
     }
 
@@ -54,7 +55,7 @@ impl Builder {
         let mut cdevs = vec![unsafe { mem::zeroed() }; self.file_ops.len()].into_boxed_slice();
         for (i, file_op) in self.file_ops.iter().enumerate() {
             unsafe {
-                bindings::cdev_init(&mut cdevs[i], &file_op.0);
+                bindings::cdev_init(&mut cdevs[i], *file_op);
                 cdevs[i].owner = &mut bindings::__this_module;
                 let rc = bindings::cdev_add(&mut cdevs[i], dev + i as bindings::dev_t, 1);
                 if rc != 0 {
--- tests/chrdev/src/lib.rs
+++ tests/chrdev/src/lib.rs
@@ -10,55 +10,42 @@ use linux_kernel_module::{self, cstr};
 struct CycleFile;
 
 impl linux_kernel_module::file_operations::FileOperations for CycleFile {
-    const VTABLE: linux_kernel_module::file_operations::FileOperationsVtable =
-        linux_kernel_module::file_operations::FileOperationsVtable::builder::<Self>()
-            .read()
-            .build();
-
     fn open() -> linux_kernel_module::KernelResult<Self> {
         Ok(CycleFile)
     }
-}
-impl linux_kernel_module::file_operations::Read for CycleFile {
-    fn read(
-        &self,
-        _file: &linux_kernel_module::file_operations::File,
-        buf: &mut linux_kernel_module::user_ptr::UserSlicePtrWriter,
-        offset: u64,
-    ) -> linux_kernel_module::KernelResult<()> {
-        for c in b"123456789"
-            .iter()
-            .cycle()
-            .skip((offset % 9) as _)
-            .take(buf.len())
-        {
-            buf.write(&[*c])?;
-        }
-        Ok(())
-    }
+
+    const READ: linux_kernel_module::file_operations::ReadFn<Self> = Some(
+        |_this: &Self,
+         _file: &linux_kernel_module::file_operations::File,
+         buf: &mut linux_kernel_module::user_ptr::UserSlicePtrWriter,
+         offset: u64|
+         -> linux_kernel_module::KernelResult<()> {
+            for c in b"123456789"
+                .iter()
+                .cycle()
+                .skip((offset % 9) as _)
+                .take(buf.len())
+            {
+                buf.write(&[*c])?;
+            }
+            Ok(())
+        },
+    );
 }
 
 struct SeekFile;
 
 impl linux_kernel_module::file_operations::FileOperations for SeekFile {
-    const VTABLE: linux_kernel_module::file_operations::FileOperationsVtable =
-        linux_kernel_module::file_operations::FileOperationsVtable::builder::<Self>()
-            .seek()
-            .build();
-
     fn open() -> linux_kernel_module::KernelResult<Self> {
         Ok(SeekFile)
     }
-}
 
-impl linux_kernel_module::file_operations::Seek for SeekFile {
-    fn seek(
-        &self,
-        _file: &linux_kernel_module::file_operations::File,
-        _offset: linux_kernel_module::file_operations::SeekFrom,
-    ) -> linux_kernel_module::KernelResult<u64> {
-        Ok(1234)
-    }
+    const SEEK: linux_kernel_module::file_operations::SeekFn<Self> = Some(
+        |_this: &Self,
+         _file: &linux_kernel_module::file_operations::File,
+         _offset: linux_kernel_module::file_operations::SeekFrom|
+         -> linux_kernel_module::KernelResult<u64> { Ok(1234) },
+    );
 }
 
 struct WriteFile {
@@ -66,42 +53,34 @@ struct WriteFile {
 }
 
 impl linux_kernel_module::file_operations::FileOperations for WriteFile {
-    const VTABLE: linux_kernel_module::file_operations::FileOperationsVtable =
-        linux_kernel_module::file_operations::FileOperationsVtable::builder::<Self>()
-            .read()
-            .write()
-            .build();
-
     fn open() -> linux_kernel_module::KernelResult<Self> {
         Ok(WriteFile {
             written: AtomicUsize::new(0),
         })
     }
-}
 
-impl linux_kernel_module::file_operations::Read for WriteFile {
-    fn read(
-        &self,
-        _file: &linux_kernel_module::file_operations::File,
-        buf: &mut linux_kernel_module::user_ptr::UserSlicePtrWriter,
-        _offset: u64,
-    ) -> linux_kernel_module::KernelResult<()> {
-        let val = self.written.load(Ordering::SeqCst).to_string();
-        buf.write(val.as_bytes())?;
-        Ok(())
-    }
-}
+    const READ: linux_kernel_module::file_operations::ReadFn<Self> = Some(
+        |this: &Self,
+         _file: &linux_kernel_module::file_operations::File,
+         buf: &mut linux_kernel_module::user_ptr::UserSlicePtrWriter,
+         _offset: u64|
+         -> linux_kernel_module::KernelResult<()> {
+            let val = this.written.load(Ordering::SeqCst).to_string();
+            buf.write(val.as_bytes())?;
+            Ok(())
+        },
+    );
 
-impl linux_kernel_module::file_operations::Write for WriteFile {
-    fn write(
-        &self,
-        buf: &mut linux_kernel_module::user_ptr::UserSlicePtrReader,
-        _offset: u64,
-    ) -> linux_kernel_module::KernelResult<()> {
-        let data = buf.read_all()?;
-        self.written.fetch_add(data.len(), Ordering::SeqCst);
-        Ok(())
-    }
+    const WRITE: linux_kernel_module::file_operations::WriteFn<Self> = Some(
+        |this: &Self,
+         buf: &mut linux_kernel_module::user_ptr::UserSlicePtrReader,
+         _offset: u64|
+         -> linux_kernel_module::KernelResult<()> {
+            let data = buf.read_all()?;
+            this.written.fetch_add(data.len(), Ordering::SeqCst);
+            Ok(())
+        },
+    );
 }
 
 struct ChrdevTestModule {
